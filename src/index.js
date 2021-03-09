751e23dc1e7f9cc329eb09f6c2a9db93c124f415
Xavi <Xavi.basets@gmail.com>
2021-03-09 10:03:27 +0100

    Tablas Multiplicar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+init();
 
-import confetti from 'canvas-confetti';
+function init() {
+  for (let i = 1; i < 11; i++) {
+    let table = document.createElement('table');
+    createMultiplicationTable(i, table);
+    document.body.appendChild(table);
+  }
+}
 
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+function createMultiplicationTable(num, table) {
+  let tbody = document.createElement('tbody');
+  for (let i = 1; i < 11; i++) {
+    let tr = document.createElement('tr');
+    // tr.appendChild(createTdMult(num, i));
+    // tr.appendChild(createTdResult(num, i));
+    tr.appendChild(createTd(`${num}` + ' x ' + `${i}`), 'mult');
+    tr.appendChild(createTd(`${num * i}`),'result')
+    tbody.appendChild(tr);
+  }
+  return table.appendChild(tbody);
+}
+
+// function createTdMult(num1, num2) {
+//   let td = document.createElement('td');
+//   td.textContent = `${num1}` + ' x ' + `${num2}`;
+//   td.classList.add('mult');
+//   return td;
+// }
+
+// function createTdResult(num1, num2) {
+//   let td = document.createElement('td');
+//   td.textContent = `${num1 * num2}`;
+//   td.classList.add('result');
+//   return td;
+// }
+function createTd(content, classToAdd){
+  let td = document.createElement('td');
+  td.textContent = content;
+  td.classList.add(classToAdd);
+  return td;
+}
