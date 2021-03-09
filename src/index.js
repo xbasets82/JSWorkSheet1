f42d5605b559d6cd4703bf9970d805e82af33243
Xavi <Xavi.basets@gmail.com>
2021-03-09 21:56:14 +0100

    proves Arrow functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-init();
-
-function init() {
+const init = ()=> {
   for (let i = 1; i < 11; i++) {
     let table = document.createElement('table');
     createMultiplicationTable(i, table);
@@ -8,35 +6,24 @@ function init() {
   }
 }
 
-function createMultiplicationTable(num, table) {
+
+const createMultiplicationTable= (num, table) =>{
   let tbody = document.createElement('tbody');
   for (let i = 1; i < 11; i++) {
     let tr = document.createElement('tr');
-    // tr.appendChild(createTdMult(num, i));
-    // tr.appendChild(createTdResult(num, i));
-    tr.appendChild(createTd(`${num}` + ' x ' + `${i}`), 'mult');
-    tr.appendChild(createTd(`${num * i}`),'result')
+    tr.appendChild(createTd(`${num}` + ' x ' + `${i}`, 'mult'));
+    tr.appendChild(createTd(`${num * i}`, 'result'));
     tbody.appendChild(tr);
   }
   return table.appendChild(tbody);
 }
 
-// function createTdMult(num1, num2) {
-//   let td = document.createElement('td');
-//   td.textContent = `${num1}` + ' x ' + `${num2}`;
-//   td.classList.add('mult');
-//   return td;
-// }
 
-// function createTdResult(num1, num2) {
-//   let td = document.createElement('td');
-//   td.textContent = `${num1 * num2}`;
-//   td.classList.add('result');
-//   return td;
-// }
-function createTd(content, classToAdd){
+const createTd = (content, classToAdd) => {
   let td = document.createElement('td');
   td.textContent = content;
-  td.classList.add(classToAdd);
+  td.classList.add('result');
   return td;
-}
+};
+
+init();
